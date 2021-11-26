c32029c966360f3ef870819d12bbec75231b6a96
Bruno Calza <brunoangelicalza@gmail.com>
2021-11-26 16:53:26 -0300

    Fixes connection hanging problem
    Connections were hanging because the Query method does not free the connection.
    Changed Query to Exec. And also added a pool of connections.

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.9.0 // indirect
+	github.com/jackc/puddle v1.2.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
--- pkg/sqlstore/impl/postgres.go
+++ pkg/sqlstore/impl/postgres.go
@@ -4,27 +4,27 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/textileio/go-tableland/pkg/sqlstore"
 )
 
 type Postgres struct {
 	ctx  context.Context
-	conn *pgx.Conn
+	pool *pgxpool.Pool
 }
 
 func (db *Postgres) Query(statement string) error {
-	_, err := db.conn.Query(db.ctx, statement)
+	_, err := db.pool.Exec(db.ctx, statement)
 	return err
 }
 
 func NewPostgres(ctx context.Context, host, port, user, pass, name string) (sqlstore.SQLStore, error) {
 	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&timezone=UTC", user, pass, host, port, name)
 
-	conn, err := pgx.Connect(ctx, databaseUrl)
+	pool, err := pgxpool.Connect(ctx, databaseUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Postgres{ctx, conn}, nil
+	return &Postgres{ctx, pool}, nil
 }
