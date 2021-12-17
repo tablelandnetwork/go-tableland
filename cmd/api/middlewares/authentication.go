1aa5c9534cdac2608d2391302f32c10e825a0bf1
Aaron Sutula <hi@asutula.com>
2021-12-17 13:12:57 -0600

    return standard error
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- cmd/api/middlewares/authentication.go
+++ cmd/api/middlewares/authentication.go
@@ -1,10 +1,12 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 
+	"github.com/textileio/go-tableland/pkg/errors"
 	"github.com/textileio/go-tableland/pkg/jwt"
 )
 
@@ -12,24 +14,28 @@ func Authentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authorization := r.Header.Get("Authorization")
 		if authorization == "" {
-			http.Error(w, "no authorization header provided", http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errors.ServiceError{Message: "no authorization header provided"})
 			return
 		}
 
 		parts := strings.Split(authorization, "Bearer ")
 		if len(parts) != 2 {
-			http.Error(w, "malformed authorization header provided", http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errors.ServiceError{Message: "malformed authorization header provided"})
 			return
 		}
 
 		j, err := jwt.Parse(parts[1])
 		if err != nil {
-			http.Error(w, fmt.Sprintf("parsing jwt: %v", err), http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errors.ServiceError{Message: fmt.Sprintf("parsing jwt: %v", err)})
 			return
 		}
 
 		if err := j.Verify(); err != nil {
-			http.Error(w, fmt.Sprintf("validating jwt: %v", err), http.StatusForbidden)
+			w.WriteHeader(http.StatusForbidden)
+			json.NewEncoder(w).Encode(errors.ServiceError{Message: fmt.Sprintf("validating jwt: %v", err)})
 			return
 		}
 
