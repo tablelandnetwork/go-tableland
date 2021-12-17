d3cb9fc23856e1064ae60fc461f10e3c0dc7192b
Aaron Sutula <hi@asutula.com>
2021-12-17 09:53:22 -0600

    auth middleware
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- cmd/api/middlewares/authentication.go
+++ cmd/api/middlewares/authentication.go
@@ -3,14 +3,35 @@ package middlewares
 import (
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/textileio/go-tableland/pkg/jwt"
 )
 
 func Authentication(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		fmt.Println(token)
+		authorization := r.Header.Get("Authorization")
+		if authorization == "" {
+			http.Error(w, "no authorization header provided", http.StatusBadRequest)
+			return
+		}
 
-		// adds token validation here
+		parts := strings.Split(authorization, "Bearer ")
+		if len(parts) != 2 {
+			http.Error(w, "malformed authorization header provided", http.StatusBadRequest)
+			return
+		}
+
+		j, err := jwt.Parse(parts[1])
+		if err != nil {
+			http.Error(w, fmt.Sprintf("parsing jwt: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if err := j.Verify(); err != nil {
+			http.Error(w, fmt.Sprintf("validating jwt: %v", err), http.StatusForbidden)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
