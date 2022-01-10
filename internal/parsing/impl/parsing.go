3dda42728e9c29b9448b0f6f213b5ef499965d35
Ignacio Hagopian <jsign.uy@gmail.com>
2022-01-10 17:23:31 -0300

    parsing: create parsing logic with some read-queries checks
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ internal/parsing/impl/parsing.go
@@ -0,0 +1,55 @@
+package impl
+
+import (
+	pg_query "github.com/pganalyze/pg_query_go/v2"
+	"github.com/textileio/go-tableland/internal/parsing"
+)
+
+type PostgresParser struct {
+}
+
+var _ parsing.Parser = (*PostgresParser)(nil)
+
+func New() *PostgresParser {
+	return &PostgresParser{}
+}
+
+func (pp *PostgresParser) ValidateCreateTable(query string) error {
+
+	panic("TODO")
+}
+
+func (pp *PostgresParser) ValidateRunSQL(query string) error {
+	panic("not implemented") // TODO: Implement
+}
+
+func (pp *PostgresParser) ValidateReadQuery(query string) error {
+	parsed, err := pg_query.Parse(query)
+	if err != nil {
+		return &parsing.ErrInvalidSyntax{InternalError: err}
+	}
+
+	if err := pp.checkSingleStatement(parsed); err != nil {
+		return err
+	}
+
+	if err := pp.checkTopLevelSelect(parsed.Stmts[0].Stmt); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pp *PostgresParser) checkSingleStatement(parsed *pg_query.ParseResult) error {
+	if len(parsed.Stmts) != 1 {
+		return &parsing.ErrNoSingleStatement{}
+	}
+	return nil
+}
+
+func (pp *PostgresParser) checkTopLevelSelect(node *pg_query.Node) error {
+	if node.GetSelectStmt() == nil {
+		return &parsing.ErrNoTopLevelSelect{}
+	}
+	return nil
+}
