01b7b5765f1fdc47ea02f531e53e82390e960d70
Ignacio Hagopian <jsign.uy@gmail.com>
2022-04-07 16:23:06 -0300

    queryfeed: progress in rearchitecture
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- pkg/queryfeed/queryfeed.go
+++ pkg/queryfeed/queryfeed.go
@@ -8,15 +8,24 @@ import (
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/rs/zerolog/log"
 	tbleth "github.com/textileio/go-tableland/pkg/tableregistry/impl/ethereum"
 )
 
+const (
+	maxLogsBatchSize = 1000
+	minChainDepth    = 5
+)
+
+type EthClient interface {
+	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+	FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error)
+}
+
 type QueryFeed struct {
-	ethClient   bind.ContractFilterer
+	ethClient   EthClient
 	scAddress   common.Address
 	contractAbi abi.ABI
 }
@@ -26,7 +35,7 @@ type MutStatement struct {
 	Statement string
 }
 
-func New(ethClient bind.ContractFilterer, scAddress common.Address) (*QueryFeed, error) {
+func New(ethClient EthClient, scAddress common.Address) (*QueryFeed, error) {
 	contractAbi, err := abi.JSON(strings.NewReader(tbleth.ContractMetaData.ABI))
 	if err != nil {
 		return nil, fmt.Errorf("get contract-abi: %s", err)
@@ -38,13 +47,65 @@ func New(ethClient bind.ContractFilterer, scAddress common.Address) (*QueryFeed,
 	}, nil
 }
 
-func (qf *QueryFeed) Start(ctx context.Context, fromHeight int64, ch chan<- interface{}) {
-	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(fromHeight),
-		Addresses: []common.Address{qf.scAddress},
+func (qf *QueryFeed) Start(ctx context.Context, fromHeight int64, ch chan<- interface{}) error {
+
+	// TODO(jsign): add mechanism to fire with the current head to avoid waiting and refactor.
+	chHeader := make(chan *types.Header)
+	subHeader, err := qf.ethClient.SubscribeNewHead(ctx, chHeader)
+	if err != nil {
+		return fmt.Errorf("subscribing to new heads: %s", err)
+	}
+	go func() {
+		defer subHeader.Unsubscribe()
+		defer close(chHeader)
+		for {
+			select {
+			case <-ctx.Done():
+				log.Info().Msg("gracefully closing new heads subscription")
+				return
+			case err := <-subHeader.Err():
+				log.Error().Err(err).Msg("new heads subscription")
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		case h, ok := <-chHeader:
+			if !ok {
+				log.Info().Msg("new head channel was closed, closing gracefully")
+				return nil
+			}
+
+			// Only make a new filter logs query if the next intended height to
+			// process is at least minChainDepth behind the reported head. This is
+			// done to avoid reorg problems.
+			if fromHeight+minChainDepth >= h.Number.Int64() {
+				continue
+			}
+
+			// Take care of putting a cap on how big the request will be.
+			toHeight := h.Number.Int64() - minChainDepth
+			if toHeight-fromHeight > maxLogsBatchSize {
+				toHeight = fromHeight + maxLogsBatchSize
+			}
+
+			query := ethereum.FilterQuery{
+				FromBlock: big.NewInt(fromHeight),
+				ToBlock:   big.NewInt(toHeight),
+				Addresses: []common.Address{qf.scAddress},
+			}
+			logs, err := qf.ethClient.FilterLogs(ctx, query)
+			if err != nil {
+				log.Error().Err(err).Msgf("filter logs from %d to %d", fromHeight, toHeight)
+				continue
+			}
+		}
 	}
 
-	sink := make(chan types.Log)
+	//////
+
 	sub, err := qf.ethClient.SubscribeFilterLogs(ctx, query, sink)
 	if err != nil {
 		return fmt.Errorf("subscribing to filtered logs: %s", err)
