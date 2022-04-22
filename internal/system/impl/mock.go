adff0dc7e57b5c1b0b074589f8f84655c9f927e5
Ignacio Hagopian <jsign.uy@gmail.com>
2022-04-22 14:45:34 -0300

    more code removal
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- internal/system/impl/mock.go
+++ internal/system/impl/mock.go
@@ -40,29 +40,6 @@ func (s *SystemMockService) GetTablesByController(ctx context.Context, controlle
 	return []sqlstore.Table{}, nil
 }
 
-// Revoke removes an address' access in the SQLStore.
-func (s *SystemMockService) Revoke(ctx context.Context, address string) error {
-	return nil
-}
-
-// IsAuthorized checks the authorization status of an address in the SQLStore.
-func (s *SystemMockService) IsAuthorized(ctx context.Context, address string) (sqlstore.IsAuthorizedResult, error) {
-	return sqlstore.IsAuthorizedResult{IsAuthorized: true}, nil
-}
-
-// GetAuthorizationRecord gets the authorization record for the provided address from the SQLStore.
-func (s *SystemMockService) GetAuthorizationRecord(
-	ctx context.Context,
-	address string,
-) (sqlstore.AuthorizationRecord, error) {
-	return sqlstore.AuthorizationRecord{Address: "some-address"}, nil
-}
-
-// ListAuthorized lists all authorization records in the SQLStore.
-func (s *SystemMockService) ListAuthorized(ctx context.Context) ([]sqlstore.AuthorizationRecord, error) {
-	return []sqlstore.AuthorizationRecord{}, nil
-}
-
 // SystemMockErrService is a dummy implementation that returns a fixed value.
 type SystemMockErrService struct {
 }
@@ -82,27 +59,4 @@ func (*SystemMockErrService) GetTableMetadata(
 // GetTablesByController returns table's fetched from SQLStore by controller address.
 func (s *SystemMockErrService) GetTablesByController(ctx context.Context, controller string) ([]sqlstore.Table, error) {
 	return []sqlstore.Table{}, errors.New("no table found")
-}
-
-// Revoke removes an address' access in the SQLStore.
-func (s *SystemMockErrService) Revoke(ctx context.Context, address string) error {
-	return errors.New("error revoking")
-}
-
-// IsAuthorized checks the authorization status of an address in the SQLStore.
-func (s *SystemMockErrService) IsAuthorized(ctx context.Context, address string) (sqlstore.IsAuthorizedResult, error) {
-	return sqlstore.IsAuthorizedResult{}, errors.New("error checking authorization")
-}
-
-// GetAuthorizationRecord gets the authorization record for the provided address from the SQLStore.
-func (s *SystemMockErrService) GetAuthorizationRecord(
-	ctx context.Context,
-	address string,
-) (sqlstore.AuthorizationRecord, error) {
-	return sqlstore.AuthorizationRecord{}, errors.New("error getting authorization record")
-}
-
-// ListAuthorized lists all authorization records in the SQLStore.
-func (s *SystemMockErrService) ListAuthorized(ctx context.Context) ([]sqlstore.AuthorizationRecord, error) {
-	return []sqlstore.AuthorizationRecord{}, errors.New("error listing authorized")
 }
