a153b38b38780c22bf88c0d71c41eabec9590f94
Ignacio Hagopian <jsign.uy@gmail.com>
2022-05-02 09:32:46 -0300

    middlewares: add chainid as a value in ctx & augment tests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/api/middlewares/contextkeys.go
+++ cmd/api/middlewares/contextkeys.go
@@ -6,4 +6,6 @@ type ContextKey int
 const (
 	// ContextKeyAddress is used to store the address of the client for the incoming request.
 	ContextKeyAddress ContextKey = iota
+	// ContextKeyChainID is used to store the chain id of the client for the incoming request.
+	ContextKeyChainID ContextKey = iota
 )
