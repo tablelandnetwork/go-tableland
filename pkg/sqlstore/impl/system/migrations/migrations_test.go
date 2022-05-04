91e691126b28a14d98caa9d75d435d4a32bd8c7b
Ignacio Hagopian <jsign.uy@gmail.com>
2022-05-04 10:50:10 -0300

    migration: add test for multi-chain migration on postgres image with data
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- pkg/sqlstore/impl/system/migrations/migrations_test.go
+++ pkg/sqlstore/impl/system/migrations/migrations_test.go
@@ -1,7 +1,47 @@
-package migrations
+package migrations_test
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stretchr/testify/require"
+	"github.com/textileio/go-tableland/internal/tableland"
+	"github.com/textileio/go-tableland/pkg/sqlstore/impl/system"
+	"github.com/textileio/go-tableland/tests"
+)
 
 func TestMultichainMigration(t *testing.T) {
-	// SELECT 'ALTER TABLE ' || table_name || ' RENAME TO _4' || table_name || ';' FROM information_schema.tables where left(table_name, 1) = '_' and left(table_name, 3) <> '_pg' \gexec
+	url := tests.PostgresURLWithImage(t, "tableland/postgres", "20220502_155944")
+	ctx := context.Background()
+	pool, err := pgxpool.Connect(ctx, url)
+	require.NoError(t, err)
+	defer pool.Close()
+
+	var execQueryCount = func(query string) int {
+		row := pool.QueryRow(ctx, query)
+		var oldNames int
+		err = row.Scan(&oldNames)
+		require.NoError(t, err)
+		return oldNames
+	}
+
+	// 1. Check that we have tables with the old (non-chainID scoped) format.
+	queryOldFormat := `SELECT count(*) FROM information_schema.tables WHERE table_name ~ '^_[0-9]+$'` // AND table_type='BASE TABLE'`
+	oldNamesCountBeforeMigration := execQueryCount(queryOldFormat)
+	require.Equal(t, 513, oldNamesCountBeforeMigration)
+
+	// 2. Boostrap system store to run the db migrations.
+	_, err = system.New(pool, tableland.ChainID(1337))
+	require.NoError(t, err)
+
+	// 3. Check that:
+	//    - We don't have *any* table with the old format.
+	//    - We have exactly the same amount of different tables with the new format as we had in point 1.
+	oldNamesCountAfterMigration := execQueryCount(queryOldFormat)
+	require.Equal(t, 0, oldNamesCountAfterMigration)
+
+	queryNewFormat := `SELECT count(*) FROM information_schema.tables WHERE table_name ~ '^_4_[0-9]+$' and table_type='BASE TABLE'`
+	newNamesCountAfterMigration := execQueryCount(queryNewFormat)
+	require.Equal(t, oldNamesCountBeforeMigration, newNamesCountAfterMigration)
 }
