0b3d12eab2e445f60150f9624309a1f27f979ee9
Ignacio Hagopian <jsign.uy@gmail.com>
2022-05-19 10:26:18 -0300

    sqlstore: rename all tables to include prefix
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- pkg/sqlstore/impl/system/migrations/016_prefix.up.sql
+++ pkg/sqlstore/impl/system/migrations/016_prefix.up.sql
@@ -1,1 +1,17 @@
 ALTER TABLE registry RENAME COLUMN name TO prefix;
+
+DO $$
+DECLARE query text;
+begin
+FOR query IN
+    SELECT 'ALTER TABLE ' || t.table_name || ' RENAME TO ' || r.prefix || t.table_name || ';' 
+FROM information_schema.tables as t
+INNER JOIN registry as r on ('_' || r.chain_id || '_' || r.id)=t.table_name
+WHERE t.table_name ~ '^_[0-9]+_[0-9]+$' AND
+      t.table_type = 'BASE TABLE'
+LOOP
+   EXECUTE query;
+END LOOP;
+
+end;
+$$
