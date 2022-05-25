803cea0afe816492af36092b17855ae1596797f4
Ignacio Hagopian <jsign.uy@gmail.com>
2022-05-25 09:54:26 -0300

    healthbot: make it multi-chain [shipit]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/healthbot/config.go
+++ cmd/healthbot/config.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/omeid/uconfig"
+	"github.com/omeid/uconfig/plugins"
+	"github.com/omeid/uconfig/plugins/file"
 )
 
+// configFilename is the filename of the config file automatically loaded.
+var configFilename = "config.json"
+
 type config struct {
-	Probe struct {
-		Target         string `default:""`
-		CheckInterval  string `default:"15s"`
-		ReceiptTimeout string `default:"20s"`
-		SIWE           string `default:""`
-		Tablename      string `default:""`
-	}
 	Metrics struct {
 		Port string `default:"9090"`
 	}
@@ -21,13 +22,34 @@ type config struct {
 		Human bool `default:"false"`
 		Debug bool `default:"false"`
 	}
+	Chains []ChainConfig
+}
+
+// ChainConfig contains probe configuration for a particular chain.
+type ChainConfig struct {
+	Name  string
+	Probe struct {
+		Target         string `default:""`
+		CheckInterval  string `default:"15s"`
+		ReceiptTimeout string `default:"20s"`
+		SIWE           string `default:""`
+		Tablename      string `default:""`
+	}
 }
 
 func setupConfig() *config {
-	conf := &config{}
+	fileBytes, err := os.ReadFile(configFilename)
+	fileStr := string(fileBytes)
+	var plugins []plugins.Plugin
+	if err != os.ErrNotExist {
+		fileStr = os.ExpandEnv(fileStr)
+		plugins = append(plugins, file.NewReader(strings.NewReader(fileStr), json.Unmarshal))
+	}
 
-	c, err := uconfig.Classic(&conf, uconfig.Files{})
+	conf := &config{}
+	c, err := uconfig.Classic(&conf, file.Files{}, plugins...)
 	if err != nil {
+		fmt.Printf("invalid configuration: %s", err)
 		c.Usage()
 		os.Exit(1)
 	}
