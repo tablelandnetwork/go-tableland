3e3a1f50a6e9f54e17816caa88cb2db8fa24442c
Ignacio Hagopian <jsign.uy@gmail.com>
2022-06-21 13:43:10 -0300

    add missing chain_id filter
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- internal/router/controllers/user.go
+++ internal/router/controllers/user.go
@@ -135,7 +135,8 @@ func (c *UserController) GetTableRow(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	stm := fmt.Sprintf("select prefix from registry where id = %s LIMIT 1", id.String())
+	chainID := vars["chainID"]
+	stm := fmt.Sprintf("select prefix from registry where id=%s and chain_id=%s LIMIT 1", id.String(), chainID)
 	prefixRow, ok := c.runReadRequest(r.Context(), stm, rw)
 	if !ok {
 		return
@@ -143,7 +144,6 @@ func (c *UserController) GetTableRow(rw http.ResponseWriter, r *http.Request) {
 
 	prefix := **prefixRow.Rows[0][0].(**string)
 
-	chainID := vars["chainID"]
 	stm = fmt.Sprintf(
 		"SELECT * FROM %s_%s_%s WHERE %s=%s LIMIT 1", prefix, chainID, id.String(), vars["key"], vars["value"])
 	rows, ok := c.runReadRequest(r.Context(), stm, rw)
