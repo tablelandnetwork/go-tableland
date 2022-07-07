55001e4007542798d456b9bc9e343f64eb651390
Aaron Sutula <asutula@users.noreply.github.com>
2022-07-07 10:48:43 -0500

    scan rows into UserData that can detect json
    Signed-off-by: Aaron Sutula <asutula@users.noreply.github.com>

--- pkg/sqlstore/store_user.go
+++ pkg/sqlstore/store_user.go
@@ -2,6 +2,8 @@ package sqlstore
 
 import (
 	"context"
+	"encoding/json"
+	"strings"
 
 	"github.com/textileio/go-tableland/pkg/parsing"
 )
@@ -15,6 +17,53 @@ type UserColumn struct {
 type UserRows struct {
 	Columns []UserColumn    `json:"columns"`
 	Rows    [][]interface{} `json:"rows"`
+}
+
+// UserData wraps data from the db that may be raw json or any other value.
+type UserData struct {
+	jsonValue  json.RawMessage
+	otherValue interface{}
+}
+
+// Value returns the underlying value.
+func (u *UserData) Value() interface{} {
+	if u.jsonValue != nil {
+		return u.jsonValue
+	}
+	return u.otherValue
+}
+
+// Scan implements Scan.
+func (u *UserData) Scan(src interface{}) error {
+	u.jsonValue = nil
+	u.otherValue = nil
+	switch src := src.(type) {
+	case string:
+		if strings.HasPrefix(src, "{") || strings.HasPrefix(src, "[") {
+			var j json.RawMessage
+			if err := json.Unmarshal([]byte(src), &j); err == nil {
+				u.jsonValue = j
+			} else {
+				u.otherValue = src
+			}
+		} else {
+			u.otherValue = src
+		}
+	case []byte:
+		tmp := src
+		u.otherValue = tmp
+	default:
+		u.otherValue = src
+	}
+	return nil
+}
+
+// MarshalJSON implements MarshalJSON.
+func (u *UserData) MarshalJSON() ([]byte, error) {
+	if u.jsonValue != nil {
+		return u.jsonValue, nil
+	}
+	return json.Marshal(u.otherValue)
 }
 
 // UserStore defines the methods for interacting with user data.
