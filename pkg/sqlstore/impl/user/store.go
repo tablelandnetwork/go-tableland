6d6cb24505d250897fbe7042a6aabae37bf4d4b0
Ignacio Hagopian <jsign.uy@gmail.com>
2022-07-13 18:07:33 -0300

    otel: automatically instrument database/sql connections [stagingdeploy]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- pkg/sqlstore/impl/user/store.go
+++ pkg/sqlstore/impl/user/store.go
@@ -5,10 +5,12 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/XSAM/otelsql"
 	_ "github.com/mattn/go-sqlite3" // sqlite3 driver
 	logger "github.com/rs/zerolog/log"
 	"github.com/textileio/go-tableland/pkg/parsing"
 	"github.com/textileio/go-tableland/pkg/sqlstore"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 var log = logger.With().Str("component", "userstore").Logger()
@@ -19,10 +21,17 @@ type UserStore struct {
 }
 
 // New creates a new UserStore.
-func New(sqliteURI string) (*UserStore, error) {
-	pool, err := sql.Open("sqlite3", sqliteURI)
+func New(dbURI string) (*UserStore, error) {
+	pool, err := otelsql.Open("sqlite3", dbURI, otelsql.WithAttributes(
+		attribute.String("name", "userstore"),
+	))
 	if err != nil {
-		return nil, fmt.Errorf("connecting to database: %s", err)
+		return nil, fmt.Errorf("connecting to db: %s", err)
+	}
+	if err := otelsql.RegisterDBStatsMetrics(pool, otelsql.WithAttributes(
+		attribute.String("name", "userstore"),
+	)); err != nil {
+		return nil, fmt.Errorf("registering dbstats: %s", err)
 	}
 	return &UserStore{
 		pool: pool,
