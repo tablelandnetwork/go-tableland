0a31ab78f29f9116807bbeec257565f0747b5b97
Ignacio Hagopian <jsign.uy@gmail.com>
2022-07-19 08:50:11 -0300

    middleware/logger: log origin ip [stagingdeploy]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- internal/router/middlewares/logging.go
+++ internal/router/middlewares/logging.go
@@ -8,14 +8,22 @@ import (
 
 // WithLogging logs requests and responses that contain useful information.
 func WithLogging(h http.Handler) http.Handler {
-	handler := func(rw http.ResponseWriter, req *http.Request) {
+	handler := func(rw http.ResponseWriter, r *http.Request) {
 		loggedRW := &responseWriterLogger{
 			ResponseWriter: rw,
 		}
-		h.ServeHTTP(loggedRW, req)
+		h.ServeHTTP(loggedRW, r)
 
 		if loggedRW.statusCode != http.StatusOK {
-			log.Warn().Int("statusCode", loggedRW.statusCode).Msg("non-200 status code response")
+			clientIP, err := extractClientIP(r)
+			if err != nil {
+				log.Warn().Err(err).Msg("can't extract client ip")
+				clientIP = "none"
+			}
+			log.Warn().
+				Int("statusCode", loggedRW.statusCode).
+				Str("clientIP", clientIP).
+				Msg("non-200 status code response")
 		}
 	}
 	return http.HandlerFunc(handler)
