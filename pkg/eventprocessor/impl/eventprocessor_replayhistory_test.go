e019ef2573b6e133586a817e60fe689001f26f45
Ignacio Hagopian <jsign.uy@gmail.com>
2022-10-03 10:01:55 -0300

    eventprocessor: add first version of replay history test

---
+++ pkg/eventprocessor/impl/eventprocessor_replayhistory_test.go
@@ -0,0 +1,164 @@
+package impl
+
+import (
+	"context"
+	"database/sql"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
+	"github.com/textileio/go-tableland/internal/tableland"
+	"github.com/textileio/go-tableland/pkg/eventprocessor"
+	"github.com/textileio/go-tableland/pkg/eventprocessor/eventfeed"
+	efimpl "github.com/textileio/go-tableland/pkg/eventprocessor/eventfeed/impl"
+	"github.com/textileio/go-tableland/pkg/eventprocessor/eventfeed/impl/sqlitechainclient"
+	executor "github.com/textileio/go-tableland/pkg/eventprocessor/impl/executor/impl"
+	"github.com/textileio/go-tableland/pkg/parsing"
+	parserimpl "github.com/textileio/go-tableland/pkg/parsing/impl"
+	"github.com/textileio/go-tableland/pkg/sqlstore/impl/system"
+	"github.com/textileio/go-tableland/tests"
+)
+
+func TestReplayProductionHistory(t *testing.T) {
+	if testing.Short() {
+		t.Skipf("skipping history replay execution because running -short tests")
+	}
+
+	historyDBURI := "file:testdata/evm_history.db?"
+
+	lastCalcChainStateHash := map[tableland.ChainID]string{}
+	for i := 0; i < 5; i++ {
+		// Launch the validator syncing all chains.
+		eps, waitFullSync := launchValidatorForAllChainsBackedByEVMHistory(t, historyDBURI)
+
+		// Wait for all of them to finish syncing.
+		waitFullSync()
+
+		// We compare the chain hash after full sync with the previous iteration calcualted hash.
+		// These should always match. If that isn't the case, it means that the chain execution is non-deterministic.
+		ctx := context.Background()
+		for _, ep := range eps {
+			bs, err := ep.executor.NewBlockScope(ctx, ep.mLastProcessedHeight.Load()+1)
+			require.NoError(t, err)
+
+			hash, err := ep.calculateHash(ctx, bs)
+			require.NoError(t, err)
+
+			if lastCalcHash, ok := lastCalcChainStateHash[ep.chainID]; ok {
+				require.Equal(t, lastCalcHash, hash, "ChainID %d is not deterministic", ep.chainID)
+			} else {
+				lastCalcChainStateHash[ep.chainID] = hash
+			}
+			require.NoError(t, bs.Close())
+		}
+
+		// Do a graceful close, to double check closing works correctly without any blocking or delays.
+		for _, ep := range eps {
+			ep.Stop()
+		}
+	}
+}
+
+func launchValidatorForAllChainsBackedByEVMHistory(t *testing.T, historyDBURI string) ([]*EventProcessor, func()) {
+	dbURI := tests.Sqlite3URI()
+	parser, err := parserimpl.New([]string{"system_", "registry", "sqlite_"})
+	require.NoError(t, err)
+
+	db, err := sql.Open("sqlite3", dbURI)
+	require.NoError(t, err)
+	db.SetMaxIdleConns(0)
+	db.SetMaxOpenConns(1)
+
+	chains := getChains(t, historyDBURI)
+	eps := make([]*EventProcessor, len(chains))
+	for i, chain := range chains {
+		eps[i] = spinValidatorStackForChainID(t, dbURI, historyDBURI, parser, chain.chainID, db)
+	}
+
+	waitForSynced := func() {
+		var wg sync.WaitGroup
+		wg.Add(len(chains))
+		for i := range chains {
+			go func(i int) {
+				defer wg.Done()
+				for {
+					if eps[i].mLastProcessedHeight.Load() == chains[i].tipBlockNumber {
+						return
+					}
+					time.Sleep(time.Second)
+				}
+			}(i)
+		}
+		wg.Wait()
+	}
+
+	return eps, waitForSynced
+}
+
+func spinValidatorStackForChainID(
+	t *testing.T,
+	dbURI string,
+	historyDBURI string,
+	parser parsing.SQLValidator,
+	chainID tableland.ChainID,
+	db *sql.DB,
+) *EventProcessor {
+	ex, err := executor.NewExecutor(chainID, db, parser, 0, &aclMock{})
+	require.NoError(t, err)
+
+	systemStore, err := system.New(dbURI, tableland.ChainID(chainID))
+	require.NoError(t, err)
+
+	eventBasedBackend, err := sqlitechainclient.New(historyDBURI, chainID)
+	require.NoError(t, err)
+
+	ef, err := efimpl.New(
+		systemStore,
+		chainID,
+		eventBasedBackend,
+		common.HexToAddress("ignored"),
+		eventfeed.WithMinBlockDepth(0))
+	require.NoError(t, err)
+
+	ep, err := New(
+		parser,
+		ex,
+		ef,
+		chainID,
+		eventprocessor.WithHashCalcStep(1_000_000_000),
+	)
+	require.NoError(t, err)
+	require.NoError(t, ep.Start())
+
+	return ep
+}
+
+type chainIDWithTip struct {
+	chainID        tableland.ChainID
+	tipBlockNumber int64
+}
+
+func getChains(t *testing.T, historyDBURI string) []chainIDWithTip {
+	db, err := sql.Open("sqlite3", historyDBURI)
+	require.NoError(t, err)
+
+	rows, err := db.Query("select chain_id, max(block_number) from system_evm_events group by chain_id")
+	require.NoError(t, err)
+	defer rows.Close()
+
+	var chains []chainIDWithTip
+	for rows.Next() {
+		require.NoError(t, rows.Err())
+		var chainID, blockNumber int64
+		require.NoError(t, rows.Scan(&chainID, &blockNumber))
+		chains = append(chains, chainIDWithTip{
+			chainID:        tableland.ChainID(chainID),
+			tipBlockNumber: blockNumber,
+		})
+	}
+
+	return chains
+}
