3408c5c9597e3edf52a70a93260675ed93dbca9d
Bruno Calza <brunoangelicalza@gmail.com>
2022-10-05 10:39:46 -0300

    cleanup is called on early returns to avoid temporarly files
    Signed-off-by: Bruno Calza <brunoangelicalza@gmail.com>

--- pkg/backup/restorer/restorer.go
+++ pkg/backup/restorer/restorer.go
@@ -7,8 +7,11 @@ import (
 	"net/http"
 	"os"
 
+	logger "github.com/rs/zerolog/log"
 	"github.com/textileio/go-tableland/pkg/backup"
 )
+
+var log = logger.With().Str("component", "backuprestorer").Logger()
 
 // BackupRestorer is responsible for restoring a database from a backup file.
 type BackupRestorer struct {
@@ -25,6 +28,11 @@ func NewBackupRestorer(url string, dst string) *BackupRestorer {
 
 // Restore restores a database from a backup file URL.
 func (br *BackupRestorer) Restore() error {
+	defer func() {
+		if err := br.cleanUp(); err != nil {
+			log.Error().Err(err).Msg("cleaning up")
+		}
+	}()
 	if err := br.downloadBackupFile(br.url, br.dst); err != nil {
 		return fmt.Errorf("download backup file: %s", err)
 	}
@@ -38,10 +46,6 @@ func (br *BackupRestorer) Restore() error {
 		return fmt.Errorf("loading the database: %s", err)
 	}
 
-	if err := br.cleanUp(); err != nil {
-		return fmt.Errorf("cleaning up: %s", err)
-	}
-
 	return nil
 }
 
@@ -51,7 +55,9 @@ func (br *BackupRestorer) downloadBackupFile(url, dst string) error {
 		return fmt.Errorf("creating backup file: %s", err)
 	}
 	defer func() {
-		_ = out.Close()
+		if err := out.Close(); err != nil {
+			log.Error().Err(err).Msg("closing")
+		}
 	}()
 
 	resp, err := http.Get(url)
@@ -59,7 +65,9 @@ func (br *BackupRestorer) downloadBackupFile(url, dst string) error {
 		return fmt.Errorf("downloading: %s", err)
 	}
 	defer func() {
-		_ = resp.Body.Close()
+		if err := resp.Body.Close(); err != nil {
+			log.Error().Err(err).Msg("closing")
+		}
 	}()
 
 	if resp.StatusCode != http.StatusOK {
@@ -80,7 +88,9 @@ func (br *BackupRestorer) load() error {
 		return fmt.Errorf("opening file: %s", err)
 	}
 	defer func() {
-		_ = in.Close()
+		if err := in.Close(); err != nil {
+			log.Error().Err(err).Msg("closing")
+		}
 	}()
 
 	out, err := os.Create(fmt.Sprintf("%s/database.db", br.dst))
@@ -88,26 +98,28 @@ func (br *BackupRestorer) load() error {
 		return fmt.Errorf("creating file: %s", err)
 	}
 	defer func() {
-		_ = out.Close()
+		if err := out.Close(); err != nil {
+			log.Error().Err(err).Msg("closing")
+		}
 	}()
 
 	_, err = io.Copy(out, in)
 	if err != nil {
 		return fmt.Errorf("copying file: %s", err)
 	}
-	return nil
-}
 
-func (br *BackupRestorer) cleanUp() error {
 	db, err := sql.Open("sqlite3", fmt.Sprintf("%s/database.db", br.dst))
 	if err != nil {
-		return fmt.Errorf("removing file: %s", err)
+		return fmt.Errorf("opening database: %s", err)
 	}
 
 	if _, err := db.Exec("DELETE FROM system_pending_tx;"); err != nil {
 		return fmt.Errorf("deleting rows from system_pending_tx file: %s", err)
 	}
+	return nil
+}
 
+func (br *BackupRestorer) cleanUp() error {
 	if err := os.Remove(fmt.Sprintf("%s/backup.db.zst", br.dst)); err != nil {
 		return fmt.Errorf("removing file: %s", err)
 	}
