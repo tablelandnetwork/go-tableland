90ce8b9ed276d91cdbb486789a17409a2aedb0a9
Ignacio Hagopian <jsign.uy@gmail.com>
2022-10-12 14:55:01 -0300

    otelhttp: add service name [stagingdeploy]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- internal/router/middlewares/otelhttp.go
+++ internal/router/middlewares/otelhttp.go
@@ -3,12 +3,23 @@ package middlewares
 import (
 	"net/http"
 
+	"github.com/textileio/go-tableland/pkg/metrics"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 // OtelHTTP wraps the handler h with OTEL metrics.
 func OtelHTTP(operation string) func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
-		return otelhttp.NewHandler(h, operation)
+		return otelhttp.NewHandler(&labeledHandler{h: h}, operation)
 	}
+}
+
+type labeledHandler struct {
+	h http.Handler
+}
+
+func (lh *labeledHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	labeler, _ := otelhttp.LabelerFromContext(r.Context())
+	labeler.Add(metrics.BaseAttrs...)
+	lh.h.ServeHTTP(rw, r)
 }
