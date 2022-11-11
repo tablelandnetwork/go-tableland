8fae3c270a274871991e2e6ac0b2ec13971adef0
Ignacio Hagopian <jsign.uy@gmail.com>
2022-11-11 17:39:59 -0300

    infra/terraform: extra fixes
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -67,12 +67,10 @@ sudo service google-cloud-ops-agent restart
 
 sudo -u validator  -i bash <<-'EOF'
 export TBLENV=$(uname -n | cut -d '-' -f 3)
-echo $TBLENV
 
 git clone https://github.com/tablelandnetwork/go-tableland.git ~/go-tableland
 cd ~/go-tableland && git checkout jsign/testnetmainnetsplit
 
-echo "~/go-tableland/${TBLENV}/hello"
 cat /tmp/.env_validator > ~/go-tableland/docker/deployed/${TBLENV}/api/.env_validator
 cat /tmp/.env_grafana > ~/go-tableland/docker/deployed/${TBLENV}/grafana/.env_grafana
 cat /tmp/.env_healthbot > ~/go-tableland/docker/deployed/${TBLENV}/healthbot/.env_healthbot
@@ -81,10 +79,12 @@ cat /tmp/grafana.db > ~/go-tableland/docker/deployed/${TBLENV}/grafana/data/graf
 mkdir ~/.ssh && chmod 700 ~/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEehs1xfBMLKpwV4sAIko++GQgauYXf5SNY4tl9ArTOG ops@textile.io" > ~/.ssh/authorized_keys
 
-echo '
+crontab - <<-MOF
 0 0 * * FRI /usr/bin/docker system prune --volumes -f  >> /home/validator/cronrun 2>&1
 10 * * * * gsutil cp "ls -A -1 /home/validator/go-tableland/docker/deployed/${TBLENV}/api/backups/*.* | tail -n 1" gs://tableland-${TBLENV}/backups/ > /home/validator/gsutil.log 2>&1"
-10 * * * * gsutil cp "ls -A -1 /home/validator/go-tableland/docker/deployed/${TBLENV}/api/backups/*.* | tail -n 1" gs://tableland-${TBLENV}/backups/tbl_backup_latest.db.zst > /home/validator/gsutil.log 2>&1' | crontab -
+10 * * * * gsutil cp "ls -A -1 /home/validator/go-tableland/docker/deployed/${TBLENV}/api/backups/*.* | tail -n 1" gs://tableland-${TBLENV}/backups/tbl_backup_latest.db.zst > /home/validator/gsutil.log 2>&1
+MOF
+
 EOF
 
 #sudo su - validator -c 'cd ~/go-tableland/docker && make testnet-up'
