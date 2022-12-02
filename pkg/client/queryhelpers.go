089e4098c03ffd0db74f6781097157a6449e771d
Ignacio Hagopian <jsign.uy@gmail.com>
2022-12-02 17:29:04 -0300

    client: progress in new api support
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ pkg/client/queryhelpers.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/textileio/go-tableland/internal/tableland"
+)
+
+// Hash validates the provided create table statement and returns its hash.
+func (c *Client) Hash(ctx context.Context, statement string) (string, error) {
+	stmt, err := c.parser.ValidateCreateTable(statement, tableland.ChainID(c.chain.ID))
+	if err != nil {
+		return "", fmt.Errorf("invalid create statement: %s", err)
+	}
+	return stmt.GetStructureHash(), nil
+}
+
+// Validate validates a write query, returning the table id.
+func (c *Client) Validate(ctx context.Context, statement string) (TableID, error) {
+	stmts, err := c.parser.ValidateMutatingQuery(statement, tableland.ChainID(c.chain.ID))
+	if err != nil {
+		return TableID{}, fmt.Errorf("invalid create statement: %s", err)
+	}
+	return NewTableID(stmts[0].GetTableID().String())
+}
---
+++ pkg/client/version.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/textileio/go-tableland/internal/router/controllers/apiv1"
+)
+
+func (c *Client) Version(ctx context.Context) (apiv1.VersionInfo, error) {
+	res, err := c.tblHTTP.Get(c.chain.Endpoint + "/version")
+	if err != nil {
+		return apiv1.VersionInfo{}, fmt.Errorf("http get error: %s", err)
+	}
+
+	var versionInfo apiv1.VersionInfo
+	if err := json.NewDecoder(res.Body).Decode(&versionInfo); err != nil {
+		return apiv1.VersionInfo{}, fmt.Errorf("decoding version info: %s", err)
+	}
+
+	return versionInfo, nil
+}
