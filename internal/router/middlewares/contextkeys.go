9c7c72836131f4a65b2c0af7ffd3f1ea2c6bd556
Joe Wagner <njwjs722@gmail.com>
2023-02-14 17:11:25 -0700

    Add some todo comments

--- internal/router/middlewares/contextkeys.go
+++ internal/router/middlewares/contextkeys.go
@@ -3,6 +3,10 @@ package middlewares
 // ContextKey is used to key context values.
 type ContextKey int
 
+// TODO: `ContextKeyAddress` is the wallet address, and since we are dropping the siwe token
+//
+//		we don't ever use, or save, this information. We should be able to remove this.
+//	 does `ContextKeyChainID` come from the siwe token? If so, we should be able to remove this.
 const (
 	// ContextKeyAddress is used to store the address of the client for the incoming request.
 	ContextKeyAddress ContextKey = iota
